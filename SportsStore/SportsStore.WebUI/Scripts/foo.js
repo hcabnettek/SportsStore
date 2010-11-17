2cc1bc281da08eed3607bd92ef45507e49930c52
ck <c_kettenbach@hotmail.com>
2010-11-16 21:31:13 -0800

    Sweet

---
+++ SportsStore/SportsStore.WebUI/Scripts/foo.js
@@ -0,0 +1,51 @@
+﻿Type.registerNamespace("foo.UI");
+
+$foo = foo.UI;
+
+
+$foo.jsonProxy = function (url) {
+
+    this.url = url;
+
+    this.invoke = function (opts) {
+
+        var o = opts || {};
+        var I = this;
+        var serviceUrl = I.url;
+
+        $.getJSON(serviceUrl, null, opts.success, opts.fail);
+    }
+
+
+}
+
+$foo.jsonProxy.registerClass("$foo.jsonProxy");
+
+$foo.Mechanic = function (name) {
+
+    this._name = name;
+    this._perId = null;
+}
+
+$foo.Mechanic.prototype = {
+
+    set_perId: function (perId) {
+        this._perId = perId;
+    },
+
+    get_perId: function () {
+        return this._perId;
+    },
+
+    set_name: function (name) {
+        this._name = name;
+    },
+
+    get_name: function () {
+        return this._name;
+    }
+
+}
+
+
+$foo.Mechanic.registerClass("$foo.Mechanic");
---
+++ SportsStore/SportsStore.WebUI/Scripts/fooDetails.js
@@ -0,0 +1,36 @@
+﻿var person = null;
+var other = null;
+
+$(function () {
+
+    LoadData();
+
+})
+
+
+function LoadData() {
+
+    debugger;
+
+    var props = { id: "user1", name: "Joe", perId: 35 };
+    person = $create("$foo.Mechanic", props, null, null, null);
+
+
+    $.getJSON(serviceUrl, null, 
+    
+        function (data, context) {
+
+            var props = { id: "user1", name: data.name, perId: data.perId };
+            person = $create("$foo.Mechanic", props, null, null, null);
+        },
+
+        function (data, context) {
+
+            alert("shoot");
+        });
+
+   
+
+}
+
+
